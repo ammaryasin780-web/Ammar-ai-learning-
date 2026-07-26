bdea9795a43c5bd93483f8951b030a14d61452ad
ammaryasin780-web <ammaryasin780@gmail.com>
2026-07-26 13:37:10 +0500

    Add alert for continue button click

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+alert("Continue button clicked");
 document.getElementById("startBtn").onclick = function () {
     document.getElementById("welcomeScreen").style.display = "none";
     document.getElementById("setupScreen").style.display = "flex";
