0ac4b112dec182b51b069780f893f570a7aab4c4
ammaryasin780-web <ammaryasin780@gmail.com>
2026-07-26 13:58:58 +0500

    Add functionality to continue button in setup

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 alert("Continue button clicked");
+
 document.getElementById("startBtn").onclick = function () {
     document.getElementById("welcomeScreen").style.display = "none";
     document.getElementById("setupScreen").style.display = "flex";
+};
+
+
+document.getElementById("continueBtn").onclick = function () {
+
+    let name = document.getElementById("studentName").value;
+    let studentClass = document.getElementById("classSelect").value;
+    let language = document.getElementById("languageSelect").value;
+
+    if (name === "" || studentClass === "") {
+        alert("Please enter your name and select class");
+        return;
+    }
+
+    document.getElementById("setupScreen").style.display = "none";
+    document.getElementById("chatScreen").style.display = "block";
+
+    console.log("Name:", name);
+    console.log("Class:", studentClass);
+    console.log("Language:", language);
 };
