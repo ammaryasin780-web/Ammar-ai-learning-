28a60326fe8c885a94bea48db2a4a87cf43d2d02
ammaryasin780-web <ammaryasin780@gmail.com>
2026-07-26 13:32:00 +0500

    Refactor event listener for start button

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-const startBtn = document.getElementById("startBtn");
-const welcomeScreen = document.getElementById("welcomeScreen");
-const setupScreen = document.getElementById("setupScreen");
-
-startBtn.addEventListener("click", () => {
-    welcomeScreen.classList.add("hidden");
-    setupScreen.classList.remove("hidden");
-});
+document.getElementById("startBtn").onclick = function () {
+    document.getElementById("welcomeScreen").style.display = "none";
+    document.getElementById("setupScreen").style.display = "flex";
+};
